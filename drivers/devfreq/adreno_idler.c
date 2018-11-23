37bdcbbbc10806fbcf033ea81ed4c20fbd6f2e70
kdrag0n <dragon@khronodragon.com>
2018-11-22 18:04:59 -0800

    adreno_idler: convert to state notifier
    Signed-off-by: kdrag0n <dragon@khronodragon.com>
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>
    Signed-off-by: Syarif Ibnu Zaelani <syarifibnuzaelani57@gmail.com>

--- drivers/devfreq/adreno_idler.c
+++ drivers/devfreq/adreno_idler.c
@@ -30,7 +30,7 @@
 
 #include <linux/module.h>
 #include <linux/devfreq.h>
-#include <linux/display_state.h>
+#include <linux/state_notifier.h>
 #include <linux/msm_adreno_devfreq.h>
 
 #define ADRENO_IDLER_MAJOR_VERSION 1
@@ -68,9 +68,6 @@ int adreno_idler(struct devfreq_dev_status stats, struct devfreq *devfreq,
 	if (!adreno_idler_active)
 		return 0;
 
-	/* Boolean to let us know if the display is on*/
-	bool display_on = is_display_on();
-
 	if (stats.busy_time < idleworkload) {
 		/* busy_time >= idleworkload should be considered as a non-idle workload. */
 		idlecount++;
@@ -86,7 +83,7 @@ int adreno_idler(struct devfreq_dev_status stats, struct devfreq *devfreq,
 			idlecount--;
 			return 1;
 		}
-	} else if (!display_on) {
+	} else if (state_suspended) {
 		/* GPU shouldn't be used for much while display is off, so ramp down the frequency */
 		*freq = devfreq->profile->freq_table[devfreq->profile->max_state - 1];
 		return 1;
